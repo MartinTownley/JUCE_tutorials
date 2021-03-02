f37e6b60280e991da31d0b1f7ae4d668e086d8d9
Martin Townley <mtown001@gold.ac.uk>
2021-03-02 19:59:54 +0000

    Added XML set state and get state to gainXML project

--- gainXML/Source/PluginProcessor.cpp
+++ gainXML/Source/PluginProcessor.cpp
@@ -170,16 +170,33 @@ juce::AudioProcessorEditor* GainXMLAudioProcessor::createEditor()
 
 //==============================================================================
 void GainXMLAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
-{
+{//scope begins
     // You should use this method to store your parameters in the memory block.
     // You could do that either as raw data, or use the XML or ValueTree classes
     // as intermediaries to make it easy to save and load complex data.
-}
+    auto state = mAPVTS.copyState();
+    // we then create a unique pointer to an xml element
+    std::unique_ptr<juce::XmlElement> xml (state.createXml());
+    //inside the bracket is what we're initialising it to, so the data that is occupying the memory.
+    
+    copyXmlToBinary(*xml, destData);
+    //retrieve the data pointed to by xml,
+    
+}//scope ends
 
 void GainXMLAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
     // You should use this method to restore your parameters from this memory block,
     // whose contents will have been created by the getStateInformation() call.
+    std::unique_ptr<juce::XmlElement> xmlState (getXmlFromBinary(data, sizeInBytes));
+    
+    if (xmlState != nullptr)
+    {
+        if (xmlState->hasTagName (mAPVTS.state.getType()))
+            {
+                mAPVTS.replaceState (juce::ValueTree::fromXml (*xmlState));
+            }
+    }
 }
 
 void GainXMLAudioProcessor::updateGain()
